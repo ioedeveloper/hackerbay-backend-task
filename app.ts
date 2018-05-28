e94407ceaf5eaac9e04a3f0d108c76b2491eacd6
ioedeveloper <ioedeveloper@DESKTOP-113GA6A>
2018-05-28 16:52:46 +0100

    Api Routing

--- app.ts
+++ app.ts
@@ -1,1 +1,13 @@
-console.log("Programmme started...");
+import express = require("express");
+import * as apiRoutes from "./routes/api";
+
+const app: express.Application = express();
+const port:any = process.env.PORT || 8001;
+
+app.use("/api", apiRoutes.apiRoutes.router);
+
+// serve the application at the given port
+app.listen(port, () => {
+    // success callback
+    console.log(`Listening at http://localhost:${port}/`);
+});
---
+++ routes/api.ts
@@ -0,0 +1,20 @@
+// importing libraries and dependencies
+import { NextFunction, Request, Response, Router } from "express";
+import * as apiController from "../controllers/api";
+
+/**
+ * Handles routing of all api request
+ */
+class Api {
+  public router: Router;
+  public constructor() {
+    this.router = Router();
+    this.init();
+  }
+  private init():void {
+    this.router.get("/", apiController.welcomeApi);
+  }
+}
+
+const apiRoutes:Api = new Api();
+export {apiRoutes};
